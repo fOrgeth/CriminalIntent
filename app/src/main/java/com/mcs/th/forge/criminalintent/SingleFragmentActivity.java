1497dff2387c20324b631be934196283d3d90221
fOrgeth <ivdovin@mail.ru>
2017-10-04 22:20:21 +0300

    -

--- app/src/main/java/com/mcs/th/forge/criminalintent/SingleFragmentActivity.java
+++ app/src/main/java/com/mcs/th/forge/criminalintent/SingleFragmentActivity.java
@@ -6,7 +6,7 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 
-public abstract class SingleFragmentActivity extends AppCompatActivity{
+public abstract class SingleFragmentActivity extends AppCompatActivity {
 
     protected abstract Fragment createFragment();
 
@@ -15,13 +15,13 @@ public abstract class SingleFragmentActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fragment);
 
-        FragmentManager fm=getSupportFragmentManager();
+        FragmentManager fm = getSupportFragmentManager();
         Fragment fragment = fm.findFragmentById(R.id.fragment_container);
 
-        if(fragment==null){
-            fragment=createFragment();
+        if (fragment == null) {
+            fragment = createFragment();
             fm.beginTransaction()
-                    .add(R.id.fragment_container,fragment)
+                    .add(R.id.fragment_container, fragment)
                     .commit();
         }
     }
