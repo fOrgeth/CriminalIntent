92a85f3ac1026d06417b66a4509ecbb00a7ecd92
fOrgeth <ivdovin@mail.ru>
2017-10-08 22:18:56 +0300

    Created CrimeListFragment layout and list item layout

--- app/src/main/java/com/mcs/th/forge/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/mcs/th/forge/criminalintent/CrimeListFragment.java
@@ -1,6 +1,30 @@
 package com.mcs.th.forge.criminalintent;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class CrimeListFragment extends Fragment {
+
+    private RecyclerView crimeRecyclerView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_crime_list, container, false);
+        crimeRecyclerView = view.findViewById(R.id.crime_recycler_view);
+        crimeRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        return view;
+    }
+
+    private class CrimeHolder extends RecyclerView.ViewHolder {
+        public CrimeHolder(LayoutInflater inflater, ViewGroup parent) {
+            super(inflater.inflate(R.layout.list_item_crime, parent, false));
+        }
+    }
 }
