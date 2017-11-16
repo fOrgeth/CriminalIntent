87705825d164acb90d416693312710649f7e0d30
fOrgeth <ivdovin@mail.ru>
2017-11-16 20:37:27 +0300

    created activity_twopane.xml refs.xml + alias

--- app/src/main/java/com/mcs/th/forge/criminalintent/CrimeListActivity.java
+++ app/src/main/java/com/mcs/th/forge/criminalintent/CrimeListActivity.java
@@ -3,8 +3,14 @@ package com.mcs.th.forge.criminalintent;
 import android.support.v4.app.Fragment;
 
 public class CrimeListActivity extends SingleFragmentActivity {
+
     @Override
     protected Fragment createFragment() {
         return new CrimeListFragment();
+    }
+
+    @Override
+    protected int getLayoutResId() {
+        return R.layout.activity_masterdetail;
     }
 }
